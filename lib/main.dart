df2c6c4cee66576837cc0a8204014aca6e054ae5
DRohan007 <drohan940@gmail.com>
2020-08-22 02:51:27 +0530

    main: Input validation for Username & Email
    Signed-off-by: DRohan007 <drohan940@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,10 @@ class myhome extends StatefulWidget {
 }
 
 class _myhomeState extends State<myhome> {
+
+  final _formkey = GlobalKey<FormState>();
+  String _username,_password= "";
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,78 +34,109 @@ class _myhomeState extends State<myhome> {
             "assets/images/bg.jpg",
             fit: BoxFit.cover,
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset(
-                  "assets/images/icon.png"
-              ),
-              Container(
-                padding: EdgeInsets.only(left: 5.0),
-                height: 50,
-                width: 300,
-                decoration: BoxDecoration(
-                  color: Colors.white70,
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.circular(20.0),
+          Form(
+            key: _formkey,
+             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                    "assets/images/icon.png"
                 ),
-                child: Center(
-                  child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "User Name",
-                    border: InputBorder.none,
-                  ),
-                ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 5.0),
-              ),
-              Container(
-                padding: EdgeInsets.only(left: 5.0),
-                height: 50,
-                width: 300,
-                decoration: BoxDecoration(
-                  color: Colors.white70,
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                child: Center(
-
-                  child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Password",
-                    border: InputBorder.none,
-                  ),
-                ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 8.0),
-                child: MaterialButton(
-                  padding: EdgeInsets.only(top: 4.0),
-                  onPressed: () {},
-                  elevation: 20.0,
-                  color: Colors.blue,
-                  splashColor: Colors.white70,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0)
-                  ),
+                Container(
+                  padding: EdgeInsets.only(left: 5.0),
                   height: 50,
-                  minWidth: 120,
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.black,
-                      letterSpacing: 3.5,
-                      fontStyle: FontStyle.normal,
-                      fontWeight: FontWeight.bold,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    color: Colors.white70,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Center(
+                    child: TextFormField(
+                      validator: (name){
+                        Pattern pattern =
+                         r'^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$';
+                         RegExp regex = new RegExp(pattern);
+                         if (regex.hasMatch(name))
+                             return null;
+                         else
+                             return 'Invalid username';
+                      },
+                    decoration: InputDecoration(
+                      hintText: "User Name",
+                      border: InputBorder.none,
+                    ),
+                    textInputAction: TextInputAction.next,
+                    onSaved: (name)=> _username = name,
+                  ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 5.0),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 5.0),
+                  height: 50,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    color: Colors.white70,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Center(
+                    child: TextFormField(
+                      obscureText: true,
+                      keyboardType: TextInputType.text,
+                      validator: (password){
+                       Pattern pattern =
+                         r'^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$';
+                       RegExp regex = new RegExp(pattern);
+                       if (regex.hasMatch(password))
+                          return null;
+                       else
+                          return 'Invalid password';
+                      },
+                    decoration: InputDecoration(
+                      hintText: "Password",
+                      border: InputBorder.none,
+                      suffixIcon: Icon(Icons.lock),
+                    ),
+                    textInputAction: TextInputAction.done,
+                    onSaved: (password)=> _password = password,
+                  ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 8.0),
+                  child: MaterialButton(
+                    padding: EdgeInsets.only(top: 4.0),
+                    onPressed: () {
+                      if(_formkey.currentState.validate()){
+                         _formkey.currentState.save();
+                       }
+                    },                   
+                    elevation: 20.0,
+                    color: Colors.blue,
+                    splashColor: Colors.white70,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)
+                    ),
+                    height: 50,
+                    minWidth: 120,
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.black,
+                        letterSpacing: 3.5,
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
